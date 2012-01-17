0dc5c5cfb8e5abd576416664ee83b83467273802
Seth Ladd <sethladd@dhcp-172-22-70-190.mtv.corp.google.com>
2012-01-17 12:56:49 -0800

    add the project

---
+++ redirector.py
@@ -0,0 +1,34 @@
+import logging
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+LIST_URL = 'http://code.google.com/p/dart/issues/list'
+NEW_URL = 'http://code.google.com/p/dart/issues/entry'
+SHOW_URL = 'http://code.google.com/p/dart/issues/detail?id='
+
+class ListIssues(webapp.RequestHandler):
+  def get(self):
+    self.redirect(LIST_URL)
+    
+class NewIssue(webapp.RequestHandler):
+  def get(self):
+    self.redirect(NEW_URL)
+    
+class ShowIssue(webapp.RequestHandler):
+  def get(self, issue_id):
+    try:
+      issue_num = int(issue_id) # validation
+      self.redirect(SHOW_URL + str(issue_num))
+    except:
+      self.redirect(NEW_URL)
+
+application = webapp.WSGIApplication([(r'/([0-9]+)', ShowIssue),
+                                      ('/new', NewIssue),
+                                      ('/', ListIssues)],
+                                     debug=True)
+
+def main():
+  run_wsgi_app(application)
+
+if __name__ == '__main__':
+  main()
