1b0be0dc8e25ffc4a08325997d26d4feeda72722
Søren Gjesse <sgjesse@google.com>
2015-06-02 09:55:28 +0200

    Add support for area redirect
    Change all matching to use RegExp.
    
    Redirect to the current staging GitHub repository for issues transfer.
    
    R=kustermann@google.com
    BUG=
    
    Review URL: https://codereview.chromium.org//1164563002

--- lib/redirect.dart
+++ lib/redirect.dart
@@ -6,41 +6,61 @@ library dartbug.com.redirect;
 
 const String gitHub = 'https://github.com';
 
-const String orginization = 'dart-lang';
+const String organization = 'dart-lang';
 
 // Test with gcloud while issues are not enabled for sdk.
-const String repository = 'gcloud';
+const String repository = 'issue_transfer_testing';
 
-final RegExp userRegExp = new RegExp(r'[A-Za-z]+');
+final RegExp issueRegExp = new RegExp(r'^/([0-9]+)$');
+final RegExp newRegExp = new RegExp(r'^/new$', caseSensitive: false);
+final RegExp userRegExp = new RegExp(r'^/([A-Za-z]+)$');
+final RegExp areaRegExp = new RegExp(r'^/area/([A-Za-z\-]+)$');
 
-final Uri showIssue = Uri.parse('$gitHub/$orginization/$repository/issues/');
-final Uri newIssue = Uri.parse('$gitHub/$orginization/$repository/issues/new');
+final Uri showIssue = Uri.parse('$gitHub/$organization/$repository/issues/');
+final Uri newIssue = Uri.parse('$gitHub/$organization/$repository/issues/new');
 final Uri userIssues =
-    Uri.parse('$gitHub/$orginization/$repository/issues/created_by/');
-final Uri listIssues = Uri.parse('$gitHub/$orginization/$repository/issues');
+    Uri.parse('$gitHub/$organization/$repository/issues/created_by/');
+final Uri areaIssues = Uri.parse('$gitHub/$organization/$repository/labels/');
+final Uri listIssues = Uri.parse('$gitHub/$organization/$repository/issues');
+
+String checkMatch(  RegExp re, String path) {
+  var match = re.firstMatch(path);
+  if (match != null) {
+    return match.group(match.groupCount);
+  } else {
+    return null;
+  }
+}
 
 /// Find the redirect for the supplied [requestUri].
 ///
 /// Returns the [Uri] to redirect to or `null` if no redirect is defined.
 Uri findRedirect(Uri requestUri) {
-  if (requestUri.pathSegments.length == 1) {
-    var segment = requestUri.pathSegments[0];
-    var issue = int.parse(segment, onError: (source) => null);
-    if (issue != null && issue > 0) {
-      return showIssue.resolve('$issue');
-    }
+  var path = requestUri.path;
+  var match;
 
-    if (segment.toLowerCase() == 'new') {
-      return newIssue;
-    }
-
-    var userMatch = userRegExp.firstMatch(segment);
-    if (userMatch != null &&
-        userMatch.start == 0 && userMatch.end == segment.length) {
-      return userIssues.resolve(segment);
-    }
-  } else if (requestUri.pathSegments.length == 0) {
+  if (requestUri.pathSegments.length == 0) {
     return listIssues;
+  }
+
+  match = checkMatch(issueRegExp, path);
+  if (match != null) {
+    return showIssue.resolve(match);
+  }
+
+  match = checkMatch(newRegExp, path);
+  if (match != null) {
+    return newIssue;
+  }
+
+  match = checkMatch(userRegExp, path);
+  if (match != null) {
+    return userIssues.resolve(match);
+  }
+
+  match = checkMatch(areaRegExp, path);
+  if (match != null) {
+    return areaIssues.resolve(match);
   }
 
   // No redirect found.
--- test/redirect_test.dart
+++ test/redirect_test.dart
@@ -9,29 +9,36 @@ import 'package:dartbug.com/redirect.dart';
 main() {
   test('issue number', () {
     expect(findRedirect(Uri.parse('http://dartbug.com/1234')).toString(),
-           '$gitHub/$orginization/$repository/issues/1234');
+           '$gitHub/$organization/$repository/issues/1234');
     expect(findRedirect(Uri.parse('http://dartbug.com/-1234')),
            isNull);
   });
 
   test('new issue', () {
     expect(findRedirect(Uri.parse('http://dartbug.com/new')).toString(),
-           '$gitHub/$orginization/$repository/issues/new');
+           '$gitHub/$organization/$repository/issues/new');
     expect(findRedirect(Uri.parse('http://dartbug.com/NEW')).toString(),
-           '$gitHub/$orginization/$repository/issues/new');
+           '$gitHub/$organization/$repository/issues/new');
   });
 
   test('user issue', () {
     expect(findRedirect(Uri.parse('http://dartbug.com/user')).toString(),
-           '$gitHub/$orginization/$repository/issues/created_by/user');
+           '$gitHub/$organization/$repository/issues/created_by/user');
     expect(findRedirect(Uri.parse('http://dartbug.com/søren')),
            isNull);
   });
 
   test('list issues', () {
     expect(findRedirect(Uri.parse('http://dartbug.com')).toString(),
-           '$gitHub/$orginization/$repository/issues');
+           '$gitHub/$organization/$repository/issues');
     expect(findRedirect(Uri.parse('http://dartbug.com/')).toString(),
-           '$gitHub/$orginization/$repository/issues');
+           '$gitHub/$organization/$repository/issues');
+  });
+
+
+  test('area', () {
+    expect(
+        findRedirect(Uri.parse('http://dartbug.com/area/Area-VM')).toString(),
+        '$gitHub/$organization/$repository/labels/Area-VM');
   });
 }
