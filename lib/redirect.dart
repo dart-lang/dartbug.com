a21a1ddd16c031273332d73ec68f5bfc98fbd340
Kevin Moore <kevmoo@google.com>
2019-05-28 14:28:42 -0700

    DRY up routing

--- lib/redirect.dart
+++ lib/redirect.dart
@@ -10,14 +10,6 @@ const gitHub = 'https://github.com';
 const organization = 'dart-lang';
 const repository = 'sdk';
 
-// Regular expressions for matching the request URI
-final issueRegExp = RegExp(r'^/([0-9]+)$');
-final newRegExp = RegExp(r'^/new$', caseSensitive: false);
-final assignedRegExp = RegExp(r'^/assigned/([A-Za-z0-9\-]+)$');
-final openedRegExp = RegExp(r'^/opened/([A-Za-z0-9\-]+)$');
-final areaRegExp = RegExp(r'^/area/([A-Za-z0-9\-]+)$');
-final triageRegExp = RegExp(r'^/triage$', caseSensitive: false);
-
 // Redirect URIs
 final _rootUri = '$gitHub/$organization/$repository';
 final _listIssues = Uri.parse('$_rootUri/issues');
@@ -25,6 +17,27 @@ final _showIssue = Uri.parse('$_rootUri/issues/');
 final _newIssue = Uri.parse('$_rootUri/issues/new');
 final _assignedIssues = Uri.parse('$_rootUri/issues/assigned/');
 final _openedIssues = Uri.parse('$_rootUri/issues/created_by/');
+
+final _matchers = <RegExp, Uri Function(String)>{
+  RegExp(r'^/([0-9]+)$'): _showIssue.resolve,
+  RegExp(r'^/new$', caseSensitive: false): (_) => _newIssue,
+  RegExp(r'^/assigned/([A-Za-z0-9\-]+)$'): _assignedIssues.resolve,
+  RegExp(r'^/opened/([A-Za-z0-9\-]+)$'): _openedIssues.resolve,
+  RegExp(r'^/area/([A-Za-z0-9\-]+)$'): (match) => _listIssues.replace(
+        queryParameters: {
+          'label': 'area-$match',
+        },
+      ),
+  RegExp(r'^/triage$', caseSensitive: false): (_) => _listIssues.replace(
+        queryParameters: {
+          'q': [
+            'is:issue',
+            'is:open',
+            ..._areaLabels.map((label) => '-label:$label'),
+          ].join(' '),
+        },
+      )
+};
 
 String _checkMatch(RegExp re, String path) {
   var match = re.firstMatch(path);
@@ -45,53 +58,15 @@ List<String> get _areaLabels => _areaLabelCache ??= List<String>.from(
 ///
 /// Returns the [Uri] to redirect to or `null` if no redirect is defined.
 Uri findRedirect(Uri requestUri) {
-  var path = requestUri.path;
-  String match;
-
   if (requestUri.pathSegments.isEmpty) {
     return _listIssues;
   }
 
-  match = _checkMatch(issueRegExp, path);
-  if (match != null) {
-    return _showIssue.resolve(match);
-  }
-
-  match = _checkMatch(newRegExp, path);
-  if (match != null) {
-    return _newIssue;
-  }
-
-  match = _checkMatch(assignedRegExp, path);
-  if (match != null) {
-    return _assignedIssues.resolve(match);
-  }
-
-  match = _checkMatch(openedRegExp, path);
-  if (match != null) {
-    return _openedIssues.resolve(match);
-  }
-
-  match = _checkMatch(areaRegExp, path);
-  if (match != null) {
-    return _listIssues.replace(
-      queryParameters: {
-        'label': 'area-$match',
-      },
-    );
-  }
-
-  match = _checkMatch(triageRegExp, path);
-  if (match != null) {
-    return _listIssues.replace(
-      queryParameters: {
-        'q': [
-          'is:issue',
-          'is:open',
-          ..._areaLabels.map((label) => '-label:$label'),
-        ].join(' '),
-      },
-    );
+  for (var entry in _matchers.entries) {
+    var match = _checkMatch(entry.key, requestUri.path);
+    if (match != null) {
+      return entry.value(match);
+    }
   }
 
   // No redirect found.
