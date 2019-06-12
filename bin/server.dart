6cfd0227b6125baf21a45d41ddc1e16141538512
Kevin Moore <kevmoo@google.com>
2019-06-11 20:29:33 -0700

    When deployed, don't double log requests, listen on anyIpv4

--- bin/server.dart
+++ bin/server.dart
@@ -13,10 +13,28 @@ Future main() async {
   // Find port to listen on from environment variable.
   var port = int.tryParse(Platform.environment['PORT'] ?? '8080');
 
+  var pipeline = const Pipeline();
+
+  final kEntries = Platform.environment.entries
+      .where((e) => e.key.startsWith('K_'))
+      .map((e) => '${e.key}\t${e.value}')
+      .toList();
+
+  InternetAddress listenAddress;
+
+  if (kEntries.isNotEmpty) {
+    print('Assuming environment is Cloud Run\n${kEntries.join('\n')}');
+    listenAddress = InternetAddress.anyIPv4;
+  } else {
+    // Only add log middleware if we're not on Cloud Run
+    pipeline = pipeline.addMiddleware(logRequests());
+    listenAddress = InternetAddress.loopbackIPv4;
+  }
+
   // Serve handler on given port.
   var server = await serve(
-    const Pipeline().addMiddleware(logRequests()).addHandler(handler),
-    InternetAddress.anyIPv4,
+    pipeline.addHandler(handler),
+    listenAddress,
     port,
   );
   print('Serving at http://${server.address.host}:${server.port}');
