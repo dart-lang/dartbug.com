9be4d416aa2d2afa5c56eabd7b0a64d335486920
Kevin Moore <kevmoo@google.com>
2023-07-21 11:45:41 -0700

    Dart 3 cleanup

--- lib/redirect.dart
+++ lib/redirect.dart
@@ -5,14 +5,12 @@
 import 'dart:convert';
 import 'dart:io';
 
-// ignore_for_file: prefer_expression_function_bodies
-
 // Environment constants
 const gitHub = 'https://github.com';
 const organization = 'dart-lang';
 const repository = 'sdk';
-const languageRepository = 'language';
-const dartBug = 'https://dartbug.com';
+const _languageRepository = 'language';
+const _dartBug = 'https://dartbug.com';
 
 // Redirect URIs
 const _rootUri = '$gitHub/$organization/$repository';
@@ -22,7 +20,7 @@ final _newIssue = Uri.parse('$_rootUri/issues/new');
 final _assignedIssues = Uri.parse('$_rootUri/issues/assigned/');
 final _openedIssues = Uri.parse('$_rootUri/issues/created_by/');
 
-const _languageRootUri = '$gitHub/$organization/$languageRepository';
+const _languageRootUri = '$gitHub/$organization/$_languageRepository';
 final _languageListIssues = Uri.parse('$_languageRootUri/issues');
 final _languageShowIssue = Uri.parse('$_languageRootUri/issues/');
 final _languageNewIssue = Uri.parse('$_languageRootUri/issues/new');
@@ -44,101 +42,89 @@ final _matchers = <RegExp, Uri Function(Match)>{
       _resolveLastChoose(_openedIssues, _languageOpenedIssues),
 
   // SDK repo only.
-  RegExp(r'^/area/([A-Za-z0-9\-]+)$'): (match) {
-    return _listIssues.replace(
-      queryParameters: {
-        'q': [
-          'label:area-${match[1]}',
-        ].join(' '),
-      },
-    );
-  },
+  RegExp(r'^/area/([A-Za-z0-9\-]+)$'): (match) => _listIssues.replace(
+        queryParameters: {
+          'q': [
+            'label:area-${match[1]}',
+          ].join(' '),
+        },
+      ),
 
   // sdk triage
-  RegExp(r'^/triage$'): (_) => Uri.parse('$dartBug/triage/sdk'),
-  RegExp(r'^/triage/sdk$', caseSensitive: false): (_) {
-    return _listIssues.replace(
-      queryParameters: {
-        'q': [
-          'is:issue',
-          'is:open',
-          ..._areaLabels.map((label) => '-label:$label'),
-        ].join(' '),
-      },
-    );
-  },
+  RegExp(r'^/triage$'): (_) => Uri.parse('$_dartBug/triage/sdk'),
+  RegExp(r'^/triage/sdk$', caseSensitive: false): (_) => _listIssues.replace(
+        queryParameters: {
+          'q': [
+            'is:issue',
+            'is:open',
+            ..._areaLabels.map((label) => '-label:$label'),
+          ].join(' '),
+        },
+      ),
 
   // core packages triage
-  RegExp(r'^/triage/core$'): (_) => Uri.parse('$dartBug/triage/core/issues'),
-  RegExp(r'^/triage/core/issues$'): (_) {
-    // Issues opened in the last 30 days not marked as bugs or enhancements.
-    return Uri.parse('$gitHub/issues').replace(
-      queryParameters: {
-        'q': [
-          'is:issue',
-          'is:open',
-          '-label:bug',
-          '-label:enhancement',
-          '-label:type-enhancement',
-          '-label:documentation',
-          'created:>$dateOneMonth',
-          ...corePackages.map((repo) => 'repo:$repo'),
-        ].join(' '),
-      },
-    );
-  },
-  RegExp(r'^/triage/core/prs$'): (_) {
-    // PRs opened in the last 30 days that haven't been assigned a reviewer and
-    // that aren't draft PRs.
-    return Uri.parse('$gitHub/issues').replace(
-      queryParameters: {
-        'q': [
-          'is:pr',
-          'is:open',
-          'review:none',
-          'draft:false',
-          'created:>$dateOneMonth',
-          ...corePackages.map((repo) => 'repo:$repo'),
-        ].join(' '),
-      },
-    );
-  },
+  RegExp(r'^/triage/core$'): (_) => Uri.parse('$_dartBug/triage/core/issues'),
+  // Issues opened in the last 30 days not marked as bugs or enhancements.
+  RegExp(r'^/triage/core/issues$'): (_) => Uri.parse('$gitHub/issues').replace(
+        queryParameters: {
+          'q': [
+            'is:issue',
+            'is:open',
+            '-label:bug',
+            '-label:enhancement',
+            '-label:type-enhancement',
+            '-label:documentation',
+            'created:>$_dateOneMonth',
+            ..._corePackages.map((repo) => 'repo:$repo'),
+          ].join(' '),
+        },
+      ),
+  // PRs opened in the last 30 days that haven't been assigned a reviewer and
+  // that aren't draft PRs.
+  RegExp(r'^/triage/core/prs$'): (_) => Uri.parse('$gitHub/issues').replace(
+        queryParameters: {
+          'q': [
+            'is:pr',
+            'is:open',
+            'review:none',
+            'draft:false',
+            'created:>$_dateOneMonth',
+            ..._corePackages.map((repo) => 'repo:$repo'),
+          ].join(' '),
+        },
+      ),
 
   // tools packages triage
-  RegExp(r'^/triage/tools$'): (_) => Uri.parse('$dartBug/triage/tools/issues'),
-  RegExp(r'^/triage/tools/issues$'): (_) {
-    // Issues opened in the last 30 days not marked as bugs or enhancements.
-    return Uri.parse('$gitHub/issues').replace(
-      queryParameters: {
-        'q': [
-          'is:issue',
-          'is:open',
-          '-label:bug',
-          '-label:enhancement',
-          '-label:type-enhancement',
-          '-label:documentation',
-          'created:>$dateOneMonth',
-          ...toolsPackages.map((repo) => 'repo:$repo'),
-        ].join(' '),
-      },
-    );
-  },
-  RegExp(r'^/triage/tools/prs$'): (_) {
-    // PRs opened in the last 30 days that haven't been assigned a reviewer and
-    // that aren't draft PRs.
-    return Uri.parse('$gitHub/issues').replace(
-      queryParameters: {
-        'q': [
-          'is:pr',
-          'is:open',
-          'review:none',
-          'draft:false',
-          'created:>$dateOneMonth',
-          ...toolsPackages.map((repo) => 'repo:$repo'),
-        ].join(' '),
-      },
-    );
-  },
+  RegExp(r'^/triage/tools$'): (_) => Uri.parse('$_dartBug/triage/tools/issues'),
+  // Issues opened in the last 30 days not marked as bugs or enhancements.
+  RegExp(r'^/triage/tools/issues$'): (_) => Uri.parse('$gitHub/issues').replace(
+        queryParameters: {
+          'q': [
+            'is:issue',
+            'is:open',
+            '-label:bug',
+            '-label:enhancement',
+            '-label:type-enhancement',
+            '-label:documentation',
+            'created:>$_dateOneMonth',
+            ..._toolsPackages.map((repo) => 'repo:$repo'),
+          ].join(' '),
+        },
+      ),
+  // PRs opened in the last 30 days that haven't been assigned a reviewer and
+  // that aren't draft PRs.
+  RegExp(r'^/triage/tools/prs$'): (_) => Uri.parse('$gitHub/issues').replace(
+        queryParameters: {
+          'q': [
+            'is:pr',
+            'is:open',
+            'review:none',
+            'draft:false',
+            'created:>$_dateOneMonth',
+            ..._toolsPackages.map((repo) => 'repo:$repo'),
+          ].join(' '),
+        },
+      ),
 };
 
 /// Resolves one of [base1] or [base2] against the last capture of `match`.
@@ -146,10 +132,8 @@ final _matchers = <RegExp, Uri Function(Match)>{
 /// Chooses [base1] if the first capture (`match[1]`) is `null`,
 /// and [base2] otherwise.
 /// (The former is an SDK repo base, the latter a language repo base.)
-Uri Function(Match) _resolveLastChoose(Uri base1, Uri base2) => (Match match) {
-      return ((match[1] == null) ? base1 : base2)
-          .resolve(match[match.groupCount]!);
-    };
+Uri Function(Match) _resolveLastChoose(Uri base1, Uri base2) => (Match match) =>
+    ((match[1] == null) ? base1 : base2).resolve(match[match.groupCount]!);
 
 final List<String> _areaLabels = List<String>.from(
   jsonDecode(
@@ -157,10 +141,10 @@ final List<String> _areaLabels = List<String>.from(
   ) as List,
 )..removeWhere((label) => !label.startsWith('area-'));
 
-final List<String> corePackages =
-    parsePackageInfo(File('static/core_packages.csv'));
-final List<String> toolsPackages =
-    parsePackageInfo(File('static/tools_packages.csv'));
+final List<String> _corePackages =
+    _parsePackageInfo(File('static/core_packages.csv'));
+final List<String> _toolsPackages =
+    _parsePackageInfo(File('static/tools_packages.csv'));
 
 /// Find the redirect for the supplied [requestUri].
 ///
@@ -183,32 +167,23 @@ Uri? findRedirect(Uri requestUri) {
 
 /// Parse a csv file with package information; return a list of repositories
 /// that we're interested in triaging.
-List<String> parsePackageInfo(File file) {
-  return file
-      .readAsLinesSync()
-      .where((line) => line.isNotEmpty)
-      .where((line) => !line.startsWith('#'))
-      .map((String line) {
-        // "args,dart-lang/args,dart.dev"
-        final info = line.split(',');
-        return PackageInfo(info[0], info[1]);
-      })
-      .where((package) => package.repo != 'dart-lang/sdk')
-      .map((package) => package.repo)
-      .toSet()
-      .toList()
-    ..sort();
-}
-
-class PackageInfo {
-  final String name;
-  final String repo;
-
-  PackageInfo(this.name, this.repo);
-}
+List<String> _parsePackageInfo(File file) => file
+    .readAsLinesSync()
+    .where((line) => line.isNotEmpty)
+    .where((line) => !line.startsWith('#'))
+    .map((String line) {
+      // "args,dart-lang/args,dart.dev"
+      final info = line.split(',');
+      return (name: info[0], repo: info[1]);
+    })
+    .where((package) => package.repo != 'dart-lang/sdk')
+    .map((package) => package.repo)
+    .toSet()
+    .toList()
+  ..sort();
 
 /// Return the current date, less ~1 month, in '2022-01-15' format.
-String get dateOneMonth {
+String get _dateOneMonth {
   final date = DateTime.now().subtract(const Duration(days: 30));
   return '${date.year}-'
       '${date.month.toString().padLeft(2, '0')}-'
